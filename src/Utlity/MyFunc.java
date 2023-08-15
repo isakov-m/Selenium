632851ab67b5a745123bbb9fcf9f598571bf9ff7
TechnoStudy <ismettechno@gmail.com>
2023-08-15 19:14:43 +0100

    Selenium Gun06 Css Ornek

--- src/Utlity/MyFunc.java
+++ src/Utlity/MyFunc.java
@@ -5,7 +5,7 @@ public class MyFunc {
     public static void Bekle(int sn){
 
         try {
-            Thread.sleep(sn*1000);
+            Thread.sleep(sn*1000l);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
