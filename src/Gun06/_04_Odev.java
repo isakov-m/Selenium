8a7eae1d3b496c814c044a5bd94649c5bc9a0594
TechnoStudy <ismettechno@gmail.com>
2023-08-17 17:26:05 +0100

    Selenium Gun06 Ödev çözümü

--- src/Gun06/_04_Odev.java
+++ src/Gun06/_04_Odev.java
@@ -9,9 +9,52 @@ package Gun06;
 // css selector
 // her bir tıklatma vs işlemin arasına MyFunc.Bekle(2);
 
-public class _04_Odev {
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+public class _04_Odev extends BaseDriver {
 
+    @Test
+    public void FormTest()
+    {
+        driver.get("https://formsmarts.com/form/yu?mode=h5");
+
+        MyFunc.Bekle(2);                                      // [id$='4586_0']
+        WebElement business=driver.findElement(By.cssSelector("[type='radio'][value='Business']"));
+        business.click();
+
+        MyFunc.Bekle(2);
+        WebElement dropDownDiscover=driver.findElement(By.cssSelector("select[id$='_4588']"));
+        dropDownDiscover.click();
+
+        MyFunc.Bekle(2);
+        WebElement optionOnlineAdvertising=driver.findElement(By.cssSelector("option[value='Online Advertising']"));
+        optionOnlineAdvertising.click();
+
+        MyFunc.Bekle(2);
+        WebElement everyDayRadio=driver.findElement(By.cssSelector("[type='radio'][value='Every Day']"));
+        everyDayRadio.click();
+
+        MyFunc.Bekle(2);
+        WebElement goodRadio=driver.findElement(By.cssSelector("[type='radio'][value='Good']"));
+        goodRadio.click();
+
+        MyFunc.Bekle(2);
+        WebElement dropdownHowLong=driver.findElement(By.cssSelector("select[id$='_4597']"));
+        dropdownHowLong.click();
+
+        MyFunc.Bekle(2);
+        WebElement option3=driver.findElement(By.cssSelector("select[id$='_4597']> :nth-child(3)"));
+        option3.click();
+
+        MyFunc.Bekle(2);
+        goodRadio.click();
+
+        BekleVeKapat();
+    }
 
 
 }
