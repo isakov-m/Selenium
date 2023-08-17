178bbe9ab55660d9fac66b1634990aa75d94200e
TechnoStudy <ismettechno@gmail.com>
2023-08-17 19:11:19 +0100

    Selenium Gun08 soru c2

--- src/Gun08/_03_Soru.java
+++ src/Gun08/_03_Soru.java
@@ -12,9 +12,11 @@ package Gun08;
 
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -31,8 +33,43 @@ public class _03_Soru extends BaseDriver {
             cookiesAccept.get(0).click();
 
 
+        MyFunc.Bekle(2);
+        WebElement btnYeniHesap=driver.findElement(By.linkText("Create new account"));
+        btnYeniHesap.click();
+
+        MyFunc.Bekle(2);
+        WebElement firstname=driver.findElement(By.name("firstname"));
+        firstname.sendKeys("ismet");
+
+        MyFunc.Bekle(2);
+        WebElement lastname=driver.findElement(By.name("lastname"));
+        lastname.sendKeys("temur");
+
+        MyFunc.Bekle(2);
+        WebElement reEmail=driver.findElement(By.name("reg_email_confirmation__"));
+        Assert.assertFalse("ReEmail gözüktü = ", reEmail.isDisplayed());
+
+        MyFunc.Bekle(2);
+        WebElement email=driver.findElement(By.name("reg_email__"));
+        email.sendKeys("ismettemur@gmail.com");
+
+        MyFunc.Bekle(2);
+        Assert.assertTrue("ReEmail gözükmedi = ", reEmail.isDisplayed());
 
 
+        WebElement webDay=driver.findElement(By.id("day"));
+        WebElement webMonth=driver.findElement(By.id("month"));
+        WebElement webYear=driver.findElement(By.id("year"));
+
+        Select day=new Select(webDay);
+        Select month=new Select(webMonth);
+        Select year=new Select(webYear);
+
+        day.selectByVisibleText("21");
+        month.selectByValue("3");
+        year.selectByVisibleText("1975");
+
+        MyFunc.Bekle(3);
         BekleVeKapat();
     }
 
