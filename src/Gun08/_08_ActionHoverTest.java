6d070ea62ad1c4af444ece92b527f0ac2dfaf2a4
TechnoStudy <ismettechno@gmail.com>
2023-08-17 20:00:23 +0100

    Selenium Gun08 Action HoverTest

---
+++ src/Gun08/_08_ActionHoverTest.java
@@ -0,0 +1,30 @@
+package Gun08;
+
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class _08_ActionHoverTest extends BaseDriver {
+
+    @Test
+    public void Test1() {
+        driver.get("https://www.hepsiburada.com/");
+
+        WebElement element = driver.findElement(By.xpath("//span[text()='Moda']"));
+        MyFunc.Bekle(2);
+
+        Actions aksiyonlar=new Actions(driver);
+        Action aksiyon= aksiyonlar.moveToElement(element).build();
+        aksiyon.perform();
+
+        MyFunc.Bekle(2);
+        BekleVeKapat();
+    }
+
+
+
+}
